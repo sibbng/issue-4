9322d535ad4846366e0ebce7c99c26024d8d50fe
Sibbngheid <sibbngheid@gmail.com>
2020-08-08 03:37:43 +0300

    fix console log

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import { useSetup, ref, watch } from "reactivue";
 function MyApp({ Component, pageProps }) {
   const state = useSetup(() => {
     const counter = ref(50);
-    watch(counter, (value) => console.log, { immediate: true });
+    watch(counter, (value) => console.log(value), { immediate: true });
     const inc = () => (counter.value += 1);
     return { counter, inc, setCount: (v) => (counter.value = v) };
   });
